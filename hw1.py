c69d055cc6dcd41be3cb6d2b1120272bd95fe7fe
Lyubov <lubsid2@hotmail.com>
2018-02-19 15:51:03 -0600

    Fixed code, added training data files.

--- hw1.py
+++ hw1.py
@@ -7,23 +7,26 @@ import copy
 import sys
 import csv
 
-
 training_data_input = [
-({'level':'Senior', 'lang':'Java', 'tweets':'no', 'phd':'no'}, False),
-({'level':'Senior', 'lang':'Java', 'tweets':'no', 'phd':'yes'}, False),
-({'level':'Mid', 'lang':'Python', 'tweets':'no', 'phd':'no'}, True),
-({'level':'Junior', 'lang':'Python', 'tweets':'no', 'phd':'no'}, True),
-({'level':'Junior', 'lang':'R', 'tweets':'yes', 'phd':'no'}, True),
-({'level':'Junior', 'lang':'R', 'tweets':'yes', 'phd':'yes'}, False),
-({'level':'Mid', 'lang':'R', 'tweets':'yes', 'phd':'yes'}, True),
-({'level':'Senior', 'lang':'Python', 'tweets':'no', 'phd':'no'}, False),
-({'level':'Senior', 'lang':'R', 'tweets':'yes', 'phd':'no'}, True),
-({'level':'Junior', 'lang':'Python', 'tweets':'yes', 'phd':'no'}, True),
-({'level':'Senior', 'lang':'Python', 'tweets':'yes', 'phd':'yes'}, True),
-({'level':'Mid', 'lang':'Python', 'tweets':'no', 'phd':'yes'}, True),
-({'level':'Mid', 'lang':'Java', 'tweets':'yes', 'phd':'no'}, True),
-({'level':'Junior', 'lang':'Python', 'tweets':'no', 'phd':'yes'}, False)
+	({'level':'Senior', 'lang':'Java', 'tweets':'no', 'phd':'no'}, False),
+	({'level':'Senior', 'lang':'Java', 'tweets':'no', 'phd':'yes'}, False),
+	({'level':'Mid', 'lang':'Python', 'tweets':'no', 'phd':'no'}, True),
+	({'level':'Junior', 'lang':'Python', 'tweets':'no', 'phd':'no'}, True),
+	({'level':'Junior', 'lang':'R', 'tweets':'yes', 'phd':'no'}, True),
+	({'level':'Junior', 'lang':'R', 'tweets':'yes', 'phd':'yes'}, False),
+	({'level':'Mid', 'lang':'R', 'tweets':'yes', 'phd':'yes'}, True),
+	({'level':'Senior', 'lang':'Python', 'tweets':'no', 'phd':'no'}, False),
+	({'level':'Senior', 'lang':'R', 'tweets':'yes', 'phd':'no'}, True),
+	({'level':'Junior', 'lang':'Python', 'tweets':'yes', 'phd':'no'}, True),
+	({'level':'Senior', 'lang':'Python', 'tweets':'yes', 'phd':'yes'}, True),
+	({'level':'Mid', 'lang':'Python', 'tweets':'no', 'phd':'yes'}, True),
+	({'level':'Mid', 'lang':'Java', 'tweets':'yes', 'phd':'no'}, True),
+	({'level':'Junior', 'lang':'Python', 'tweets':'no', 'phd':'yes'}, False)
 ] 
+
+test_sample1 = {"level" : "Junior","lang" : "Java","tweets" : "yes","phd" : "no"}
+test_sample2 = {"level" : "Junior","lang" : "Java","tweets" : "yes","phd" : "yes"} 
+
 
 def read_data(training_data):
 	training_data_new = []
@@ -34,7 +37,7 @@ def read_data(training_data):
 
 	for record in training_data:
 		new_record = record[0]
-		new_record['hire'] = record[1]
+		new_record['class'] = record[1]
 		training_data_new.append(new_record)
 
 	training_data_frame = pd.DataFrame(training_data_new)
@@ -45,60 +48,39 @@ def read_data(training_data):
 			values.append(value)
 		attributes[key] = values
 
-	print(build_tree(training_data_frame, attributes))
-	
+	final_dt = (build_tree(training_data_frame, attributes))
+	print (final_dt)
+	classify_input = classify(final_dt, test_sample1)
+	# classify(final_dt, test_sample2)
+	# print (classify_input)
 
 def build_tree(training_data_frame, attributes):
-	all_true = True
-	all_false = True
+	num_true = 0
+	num_false = 0
 	entropies = {}
 	tree = ()
 
-
-	#Check to see if all the reocrd have the same class
-	#If they do, then return that class
-	for value in training_data_frame['hire']:
+	# count number of values in each class
+	for value in training_data_frame['class']:
 		if value == True:
-			all_false = False
-		else:
-			all_true = False
-	if all_false == True:
+			num_true += 1
+		else :
+			num_false +=1
+
+	# if all values of class are true or false return that value
+	if num_true == 0:
 		return False
-	elif all_true == True:
+	elif num_false == 0:
 		return True
-	
 
-	#Check to see if there is an attribute left to split on,
-	#if not return the most common class
+	# if there are no attributes left return the most common class
 	if len(attributes) == 0:
-		num_true = 0
-		num_false = 0
-
-		for value in training_data_frame['hire']:
-			if value == True:
-				num_true += 1
-			else:
-				num_false +=1
-
-		if num_true > num_false:
+		if num_false > num_true:
+			return False
+		else: 
 			return True
 
-		return False
-
-	#if there is only on attribute left	
 	sub_tree = {}
-	if len(attributes) == 1:
-		
-		#Split on the last attribute
-		for key, values in attributes.items():
-			attributes.popitem()
-			for value in values:
-				sub_tree[value] = build_tree(copy.deepcopy(training_data_frame.loc[training_data_frame[key] == value]), copy.deepcopy(attributes))
-				
-			tree = (key, sub_tree)
-		#print(tree)	
-		return tree
-
 
 	#If there is more than 1 attribute left calculate the entropy for each attribute 
 	for key, value in attributes.items():
@@ -117,15 +99,20 @@ def build_tree(training_data_frame, attributes):
 	for value in attribute_values:
 		sub_tree[value] = build_tree(copy.deepcopy(training_data_frame.loc[training_data_frame[highest_attribute] == value]), copy.deepcopy(attributes))
 	
+	# add default to end of subtree 
+	if num_true > num_false:
+		sub_tree['None'] = True
+	else:
+		sub_tree['None'] = False
+
 	tree = (highest_attribute, sub_tree)
 	return tree
 
 
-
 def calc_entropy(training_data_frame, attribute):
 	total_num_records = len(training_data_frame.index)
-	num_true = len(training_data_frame.loc[training_data_frame['hire'] == True])
-	num_false = len(training_data_frame.loc[training_data_frame['hire'] == False])
+	num_true = len(training_data_frame.loc[training_data_frame['class'] == True])
+	num_false = len(training_data_frame.loc[training_data_frame['class'] == False])
 	prob_false = float(num_false/total_num_records)
 	prob_true = float(num_true/total_num_records)
 	gain = 0
@@ -137,11 +124,12 @@ def calc_entropy(training_data_frame, attribute):
 
 	for key, values in attribute.items():
 		for value in values:
+			# num_records is the number of 
 			num_records = len(training_data_frame.loc[training_data_frame[key] == value])
 			if num_records < 0.0000000000001:
 				continue
-			num_true = len(training_data_frame.loc[(training_data_frame['hire'] == True) & (training_data_frame[key] == value)])
-			num_false = len(training_data_frame.loc[(training_data_frame['hire'] == False) & (training_data_frame[key] == value)])
+			num_true = len(training_data_frame.loc[(training_data_frame['class'] == True) & (training_data_frame[key] == value)])
+			num_false = len(training_data_frame.loc[(training_data_frame['class'] == False) & (training_data_frame[key] == value)])
 			prob_false = float(num_false/num_records)
 			prob_true = float(num_true/num_records)
 
@@ -158,7 +146,6 @@ def calc_entropy(training_data_frame, attribute):
 # Function which accepts an input data file and parses it into an
 # acceptable format for the read_data() function.
 def read_file_data(input_file_name):
-
 	# File is read using pandas, header line included.
     input_data = pd.read_csv(input_file_name, header=0)	
     # Creates list with each line of file placed into a dictionary.
@@ -180,10 +167,8 @@ def read_file_data(input_file_name):
     # Sends newly constructed list to read_data () method
     read_data(final_dict_list)
 
-	
-def classify(sample):
+def classify(dt, sample):
 	return True
-
 
 if len(sys.argv) > 1:
     input_file_name = sys.argv[1]
