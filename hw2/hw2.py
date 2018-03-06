94ed07d02ce4683e517d025462876f6cb7d5e297
Caleb <calsutton728@gmail.com>
2018-03-06 09:29:51 -0600

    final touches

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -1,15 +1,27 @@
-import sys
-import csv
-import math
-import time
+""" 
+Program: hw2.py
+Programmed By: Caleb Sutton
+Description: Implementation of KNN algorithm on MNIST
+Trace Folder: Sutton728/hw2
+"""
 
+# imports
+import sys     # used for csv input
+import csv     # used for csv input
+import math    # used for sqrt() in calculateDistance()
+import time    # used to time algorithm
+
+# program main
 def main():
+     # Check to ensure program is being used properly
      if (len(sys.argv) != 4):
           print("\nUsage: python hw2.py <trainingDataFile> <testDataFile> <k>\n")
           exit()
 
+     # Start a timer for measuring program speed/performance
      startTime = time.time()
 
+     # Variables used in main
      trainingData = []
      testData = []
      k = 0
@@ -17,52 +29,72 @@ def main():
      numIncorrect = 0
      accuracyRate = 0.0
 
+     # Initialize training data, test data, and K
      trainingData = readCSV(sys.argv[1])
      testData = readCSV(sys.argv[2])
      k = int(sys.argv[3])
 
+     # Print k
      print('\nK = ' + str(k) + '\n')
 
+     # For each test sample in the testing data call the classify
+     # sample function, keeping track of the number if correct
+     # and incorrect samples
      for testSample in testData[1:]:
           if classifySample(trainingData[1:], testSample, k) == True:
                numCorrect += 1
           else:
                numIncorrect += 1
 
+     # Compute the accuracy
      accuracyRate = numCorrect / (numCorrect + numIncorrect)
      round(accuracyRate, 4)
 
+     # Print results
      print('\nAccuracy Rate: ' + str(accuracyRate * 100) + '%')
      print('Number of misclassified test samples: ' + str(numIncorrect))
      print('Total number of test samples: ' + str(numCorrect + numIncorrect) + '\n')
 
+     # Print time elapsed
      endTime = time.time()
      print('Time Elapsed: ' + str(round((endTime-startTime), 3)) + 's\n')
 
-def readCSV(filepath):
-     data = []
 
-     with open(filepath) as csvfile:
-          readCSV = csv.reader(csvfile, delimiter = ',')
-          
-          for row in readCSV:
-               data.append(row)
 
-     return data
 
+
+# classifySample() function takes a set of training data, test
+# sample, and k value as parameters and returns whether or 
+# not the test samples computed class mathces its actual
+# class
 def classifySample(trainingData, testSample, k):
+     # list for holding the nearest neighbors to the test sample
      nearestSamples = []
-     
 
+     # loop for each sample in the training data and calculate
+     # its distance from the test sample and maintain a sorted
+     # list of length k of the nearest neighbors
      for trainingSample in trainingData:
+          # result is a dictionary used for keeping track of 
+          # the nearest neighbors
           result = {}
           result['class'] = trainingSample[0]
           result['distance'] = calculateDistance(testSample[1:], trainingSample[1:])
-          result['vote'] = 1/result['distance']
+          if result['distance'] == 0:
+               result['vote'] = 1
+          else:
+               result['vote'] = 1/result['distance']
 
+          # if there are less than k samples in the list append
+          # results and then sort the array
           if len(nearestSamples) < k:
                nearestSamples.append(result)
                nearestSamples.sort(key = lambda sample: sample['distance'], reverse = False)
+          # we only get to this else once the list contains k 
+          # samples, which means we can iterate through and insert
+          # the new sample in the correct postion, if its distance
+          # is larger than all the samples it will be immediately
+          # popped, otherwise the greatest distance will be popped
           else:
                i = 0
                for sample in nearestSamples:
@@ -72,16 +104,20 @@ def classifySample(trainingData, testSample, k):
                          break
                     i += 1
 
+     # call organizational function calculateClass() to calculate
+     # the class of the test sample using the nearestSamples list
      computedClass = calculateClass(nearestSamples)
      print('Desired Class: ' + str(testSample[0]) + ', Computed Class: ' + str(computedClass))
-     
 
+     # return true if the calculated class mathces the actual class
+     # otherwise return false
      if int(computedClass) == int(testSample[0]):
           return True
      else:
           return False
 
-
+# calculateDistance() takes two samples as parameters and returns
+# the calculated euclidean distance bewteen the two
 def calculateDistance(sample1, sample2):
      distance = 0
 
@@ -91,6 +127,9 @@ def calculateDistance(sample1, sample2):
 
      return math.sqrt(distance)
 
+# calculateClass() is a helper function to classifySample() it
+# takes the nearestSamples list and then computes which class
+# had the highest number of votes and returns it
 def calculateClass(nearestSamples):
      votes = [0,0,0,0,0,0,0,0,0,0]
      highest = 0
@@ -105,5 +144,18 @@ def calculateClass(nearestSamples):
                highest = i
      
      return highest
+
+# readCSV() takes a file path to a .csv file as a parameter
+# and returns a list of lists representing the csv file
+def readCSV(filepath):
+     data = []
+
+     with open(filepath) as csvfile:
+          readCSV = csv.reader(csvfile, delimiter = ',')
+          
+          for row in readCSV:
+               data.append(row)
+
+     return data
 
 main()
--- hw2/hw2_parallel.py
+++ hw2/hw2_parallel.py
@@ -1,16 +1,28 @@
-import sys
-import csv
-import math
-import time
-import multiprocessing as mp
+""" 
+Program: hw2_parallel.py
+Programmed By: Caleb Sutton
+Description: Parallelized implementation of KNN algorithm on MNIST
+Trace Folder: Sutton728/hw2
+"""
 
+# imports
+import sys     # used for csv input
+import csv     # used for csv input
+import math    # used for sqrt() in calculateDistance()
+import time    # used to time algorithm
+import multiprocessing as mp  # used to implement multiprocessing
+
+# program main
 def main():
+     # Check to ensure program is being used properly
      if (len(sys.argv) != 4):
           print("\nUsage: python hw2_parallel.py <trainingDataFile> <testDataFile> <k>\n")
           exit()
 
+     # Start a timer for measuring program speed/performance
      startTime = time.time()
 
+      # Variables used in main
      trainingData = []
      testData = []
      k = 0
@@ -18,22 +30,29 @@ def main():
      numIncorrect = 0
      accuracyRate = 0.0
 
+     # Initialize training data, test data, and K
      trainingData = readCSV(sys.argv[1])
      testData = readCSV(sys.argv[2])
      k = int(sys.argv[3])
 
+     # print K
      print('\nK = ' + str(k) + '\n')
 
+     # variable specific to multiprocessing
      que = mp.Queue()
      cpuCount = mp.cpu_count()
      samplesPerCore = [0 for i in range(cpuCount)]
      processes = []
 
+     # calculate the number of samples to be classified by
+     # each core
      for i in range(cpuCount):
           samplesPerCore[i] = len(testData[1:]) / cpuCount
           if i < len(testData[1:]) % cpuCount:
                samplesPerCore[i] += 1
 
+     # start each process using the classifySamples() function
+     # giving it the training data, and a chunk of the test samples
      startIndex = 1
      for i in range(cpuCount):
           samples = testData[int(startIndex) : int(startIndex + samplesPerCore[i])]
@@ -42,9 +61,11 @@ def main():
           p.start()
           startIndex += samplesPerCore[i]
 
+     # wait for processes to finish
      for p in processes:
           p.join()
 
+     # get the results from each process using the que
      for i in range(que.qsize()):
           result = que.get()
           if result == True:
@@ -52,40 +73,57 @@ def main():
           else:
                numIncorrect += 1
 
+     # compute the accuracy
      accuracyRate = numCorrect / (numCorrect + numIncorrect)
      round(accuracyRate, 4)
 
+     # print the results
      print('\nAccuracy Rate: ' + str(accuracyRate * 100) + '%')
      print('Number of misclassified test samples: ' + str(numIncorrect))
      print('Total number of test samples: ' + str(numCorrect + numIncorrect) + '\n')
 
+     # print the time elapsed
      endTime = time.time()
      print('Time Elapsed: ' + str(round((endTime-startTime), 3)) + 's\n')
 
-def readCSV(filepath):
-     data = []
 
-     with open(filepath) as csvfile:
-          readCSV = csv.reader(csvfile, delimiter = ',')
-          
-          for row in readCSV:
-               data.append(row)
 
-     return data
 
+
+# classifySamples() function takes a set of training data, test
+# samples, k value, and a que as parameters. it adds a true to
+# the que if it correctly classifies a sample, and a false
+# if it incorrectly classifies a sample
 def classifySamples(trainingData, testSamples, k, que):
+     # loop through each sample in the list of test samples
      for testSample in testSamples:
+          # list for holding nearest neighbors
           nearestSamples = []
           
+          # loop for each sample in the training data and calculate
+          # its distance from the test sample and maintain a sorted
+          # list of length k of the nearest neighbors
           for trainingSample in trainingData:
+               # result is a dictionary used for keeping track of 
+               # the nearest neighbors
                result = {}
                result['class'] = trainingSample[0]
                result['distance'] = calculateDistance(testSample[1:], trainingSample[1:])
-               result['vote'] = 1/result['distance']
+               if result['distance'] == 0:
+                    result['distance'] = 999999
+               else:
+                    result['vote'] = 1/result['distance']
 
+               # if there are less than k samples in the list append
+               # results and then sort the array
                if len(nearestSamples) < k:
                     nearestSamples.append(result)
                     nearestSamples.sort(key = lambda sample: sample['distance'], reverse = False)
+               # we only get to this else once the list contains k 
+               # samples, which means we can iterate through and insert
+               # the new sample in the correct postion, if its distance
+               # is larger than all the samples it will be immediately
+               # popped, otherwise the greatest distance will be popped
                else:
                     i = 0
                     for sample in nearestSamples:
@@ -95,15 +133,20 @@ def classifySamples(trainingData, testSamples, k, que):
                               break
                          i += 1
 
+          # call organizational function calculateClass() to calculate
+          # the class of the test sample using the nearestSamples list
           computedClass = calculateClass(nearestSamples)
           print('Desired Class: ' + str(testSample[0]) + ', Computed Class: ' + str(computedClass))
 
+          # add true if the calculated class mathces the actual class
+          # otherwise add false
           if int(computedClass) == int(testSample[0]):
                que.put(True)
           else:
                que.put(False)
 
-
+# calculateDistance() takes two samples as parameters and returns
+# the calculated euclidean distance bewteen the two
 def calculateDistance(sample1, sample2):
      distance = 0
 
@@ -113,6 +156,9 @@ def calculateDistance(sample1, sample2):
 
      return math.sqrt(distance)
 
+# calculateClass() is a helper function to classifySample() it
+# takes the nearestSamples list and then computes which class
+# had the highest number of votes and returns it
 def calculateClass(nearestSamples):
      votes = [0,0,0,0,0,0,0,0,0,0]
      highest = 0
@@ -127,6 +173,19 @@ def calculateClass(nearestSamples):
                highest = i
      
      return highest
+
+# readCSV() takes a file path to a .csv file as a parameter
+# and returns a list of lists representing the csv file
+def readCSV(filepath):
+     data = []
+
+     with open(filepath) as csvfile:
+          readCSV = csv.reader(csvfile, delimiter = ',')
+          
+          for row in readCSV:
+               data.append(row)
+
+     return data
 
 if __name__ == "__main__":
      main()
